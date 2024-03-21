f0b6ee14be03d87c24f6699bd7b6fe6e09e9b09e
Obi Juan <hola@obijuan.dev>
2024-03-21 10:43:10 -0500

    Clean and tidy up PHP code with relevant useful comments, moves the plugin initialization one step before activation, and version bump.

--- includes/PostTypes.php
+++ includes/PostTypes.php
@@ -5,17 +5,47 @@ namespace ObiExcludeFromSearch\Includes;
 use WP_REST_Request;
 use WP_REST_Response;
 
+/**
+ * PostTypes
+ *
+ * Class to handle post types
+ *
+ * @since 1.0.0
+ */
 class PostTypes {
 
+	/**
+	 * The PostTypes instance.
+	 *
+	 * @var PostTypes
+	 * @access private
+	 * @static
+	 * @since 1.0.0
+	 */
 	private static $instance;
 
+	/**
+	 * Initialize the PostTypes logic.
+	 */
 	private function __construct() {
+
+		// Register post types endpoint.
 		add_action( 'rest_api_init', array( __CLASS__, 'register_post_types_endpoints' ) );
+
+		// Adjust the search query.
 		add_action( 'pre_get_posts', array( __CLASS__, 'adjust_search_query' ) );
+
+		// Handle new and removed post types.
 		add_action( 'registered_post_type', array( __CLASS__, 'handle_new_post_type' ), 10, 2 );
 		add_action( 'unregistered_post_type', array( __CLASS__, 'handle_removed_post_type' ), 10, 1 );
 	}
 
+	/**
+	 * Get the instance of the class.
+	 *
+	 * @since 1.0.0
+	 * @return PostTypes
+	 */
 	public static function get_instance() {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new self();
@@ -24,7 +54,21 @@ class PostTypes {
 		return self::$instance;
 	}
 
+	/**
+	 * Register the post types endpoint.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 * @access public
+	 * @static
+	 */
 	public static function register_post_types_endpoints() {
+
+		/**
+		 * Register the post types endpoint
+		 *
+		 * Usage : /wp-json/obiRCPT/v1/post-types
+		 */
 		register_rest_route(
 			'obiRCPT/v1',
 			'/post-types',
@@ -35,6 +79,11 @@ class PostTypes {
 			)
 		);
 
+		/**
+		 * Register the update post type status endpoint
+		 *
+		 * Usage : /wp-json/obiRCPT/v1/update-post-type-status
+		 */
 		register_rest_route(
 			'obiRCPT/v1',
 			'/update-post-type-status',
@@ -49,6 +98,14 @@ class PostTypes {
 		);
 	}
 
+	/**
+	 * Get the post types callback.
+	 *
+	 * @since 1.0.0
+	 * @return array. The post types with their corresponding statuses.
+	 * @access public
+	 * @static
+	 */
 	public function get_post_types_callback() {
 		$args = array(
 			'public' => true,
@@ -73,6 +130,16 @@ class PostTypes {
 		);
 	}
 
+	/**
+	 * Update the post type status callback.
+	 *
+	 * @param WP_REST_Request $request. The request object.
+	 *
+	 * @since 1.0.0
+	 * @return WP_REST_Response. The response message.
+	 * @access public
+	 * @static
+	 */
 	public function update_post_type_status_callback( WP_REST_Request $request ) {
 		// Extract the new post type status from the request!
 		$post_type_status = $request->get_json_params();
@@ -83,6 +150,17 @@ class PostTypes {
 		return new WP_REST_Response( 'Post type status updated successfully.', 200 );
 	}
 
+	/**
+	 * Initialize the post type statuses.
+	 *
+	 * This function is called when the plugin is activated.
+	 * It initializes the post type statuses in the database.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 * @access public
+	 * @static
+	 */
 	public static function initialize_post_type_statuses() {
 		$initialized = get_option( 'obiRCPT_post_type_statuses_initialized', false );
 
@@ -105,6 +183,14 @@ class PostTypes {
 		}
 	}
 
+	/**
+	 * Adjust the search query for post types.
+	 *
+	 * @param WP_Query $query. The query object.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
 	public static function adjust_search_query( $query ) {
 		if ( ! is_admin() && $query->is_main_query() && $query->is_search ) {
 			$post_type_statuses = get_option( 'obiRCPT_post_type_statuses', array() );
@@ -120,20 +206,37 @@ class PostTypes {
 		}
 	}
 
+	/**
+	 * Handle new added post types.
+	 *
+	 * @param string $post_type. The post type.
+	 * @param array  $args. The post type arguments.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
 	public static function handle_new_post_type( $post_type, $args ) {
 		$post_type_statuses = get_option( 'obiRCPT_post_type_statuses', array() );
 
-		// Only add the new post type if it's not already in the array!
+		// Only add the new post type if it's not already in the array of existing post types.
 		if ( ! isset( $post_type_statuses[ $post_type ] ) ) {
-			$post_type_statuses[ $post_type ] = true;  // Or any other default value you want!
+			$post_type_statuses[ $post_type ] = true;  // Or any other default value.
 			update_option( 'obiRCPT_post_type_statuses', $post_type_statuses );
 		}
 	}
 
+	/**
+	 * Handle removed post types.
+	 *
+	 * @param string $post_type. The post type.
+	 *
+	 * @since 1.0.0
+	 * @return void
+	 */
 	public static function handle_removed_post_type( $post_type ) {
 		$post_type_statuses = get_option( 'obiRCPT_post_type_statuses', array() );
 
-		// Only update the option if the post type is in the array!
+		// Only update the option if the post type is in the array.
 		if ( isset( $post_type_statuses[ $post_type ] ) ) {
 			unset( $post_type_statuses[ $post_type ] );
 			update_option( 'obiRCPT_post_type_statuses', $post_type_statuses );
--- obi-exclude-from-search.php
+++ obi-exclude-from-search.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Obi Exclude from Search
  * Description: Include or exclude (custom) post types from the WordPress search feature.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Obi Juan <hola@obijuan.dev>
  * Author URI: https://obijuan.dev
  * Plugin URI: https://obijuan.dev/obi-remove-post-types-from-search
@@ -34,8 +34,22 @@ use ObiExcludeFromSearch\Includes\PostTypes;
 final class Obi_Init {
 
 
+	/**
+	 * Instance
+	 *
+	 * @var Obi_Init
+	 * @access private
+	 * @static
+	 * @since 1.0.0
+	 */
 	private static $instance;
 
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * @since 1.0.0
+	 */
 	private function __construct() {
 
 		self::define_constants();
@@ -43,6 +57,12 @@ final class Obi_Init {
 		self::check_minimum_php_version();
 	}
 
+	/**
+	 * Get the instance of the class.
+	 *
+	 * @since 1.0.0
+	 * @return Obi_Init
+	 */
 	public static function get_instance() {
 
 		if ( ! isset( self::$instance ) ) {
@@ -52,9 +72,15 @@ final class Obi_Init {
 		return self::$instance;
 	}
 
+
+	/**
+	 * Define constants
+	 *
+	 * @since 1.0.0
+	 */
 	private static function define_constants() {
 
-		define( 'OBI_EXCLUDE_FROM_SEARCH_VERSION', '1.0.0' );
+		define( 'OBI_EXCLUDE_FROM_SEARCH_VERSION', '1.0.1' );
 		define( 'OBI_EXCLUDE_FROM_SEARCH_TEXTDOMAIN', 'obi-exclude-from-search' );
 		define( 'OBI_EXCLUDE_FROM_SEARCH_DIRNAME', plugin_basename( __DIR__ ) );
 		define( 'OBI_EXCLUDE_FROM_SEARCH_FILE', __FILE__ );
@@ -64,19 +90,39 @@ final class Obi_Init {
 		define( 'OBI_EXCLUDE_FROM_SEARCH_MINIMUM_PHP_REQUIREMENT', '7.4' );
 	}
 
+
+	/**
+	 * Load the plugin
+	 *
+	 * Load the classes we depend on and initialize the plugin.
+	 *
+	 * @since 1.0.0
+	 */
 	public static function load_obi_plugin() {
 		// On plugins loaded...
 		AdminPage::get_instance();
 		PostTypes::get_instance();
 	}
 
+
+	/**
+	 * Activate the plugin
+	 *
+	 * @since 1.0.0
+	 */
 	public static function activate() {
 
 		// On plugin activation...
 
+		// Initialize post type statuses.
 		PostTypes::initialize_post_type_statuses();
 	}
 
+	/**
+	 * Deactivate the plugin
+	 *
+	 * @since 1.0.0
+	 */
 	public static function deactivate() {
 
 		// On plugin deactivation...
@@ -100,6 +146,14 @@ final class Obi_Init {
 		return version_compare( phpversion(), self::obi_exclude_from_search_minimum_php_requirement(), '>=' );
 	}
 
+
+	/**
+	 * Check if PHP version meets minimum requirements.
+	 *
+	 * Enqueue admin notice if not.
+	 *
+	 * @since 1.0.0
+	 */
 	public static function check_minimum_php_version() {
 
 		// Ensuring our PHP version requirement is met first before loading the Obi Exclude From Search plugin.
@@ -113,7 +167,6 @@ final class Obi_Init {
 			return;
 		}
 	}
-
 
 	/**
 	 * Admin notice for PHP version requirement.
@@ -139,8 +192,27 @@ final class Obi_Init {
 
 
 
-
+/**
+ * Get the instance of the class.
+ *
+ * @since 1.0.0
+ * @return Obi_Init
+ */
 $obi_plugin = Obi_Init::get_instance();
 
-register_activation_hook( OBI_EXCLUDE_FROM_SEARCH_FILE, array( $obi_plugin, 'activate' ) );
+/**
+ * Load the classes we depend on to run the plugin.
+ *
+ * Load - AdminPage.
+ * Load - PostTypes.
+ *
+ * @since 1.0.0
+ */
 add_action( 'plugins_loaded', array( $obi_plugin, 'load_obi_plugin' ) );
+
+/**
+ * Activate the plugin
+ *
+ * @since 1.0.0
+ */
+register_activation_hook( OBI_EXCLUDE_FROM_SEARCH_FILE, array( $obi_plugin, 'activate' ) );
